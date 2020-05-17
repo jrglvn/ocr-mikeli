ff5f92a940f8da0ab1d12e5c31f4ec0fa34978f0
jure <jrglvn@yahoo.com>
2020-05-17 22:03:34 +0200

    initial

---
+++ readme.md
@@ -0,0 +1,5 @@
+add to windows system variables
+restart PC
+
+variable_name: GOOGLE_APPLICATION_CREDENTIALS
+variable_value: "path to ocrmikeli-2c46790fa7af.json"
---
+++ server/index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
+const bodyParser = require("body-parser");
+const fileUpload = require("express-fileupload");
+const cors = require("cors");
+const parsePDF = require("./vision");
+const app = express();
+const upload = multer({});
+const port = 3001;
+const API_KEY = "AIzaSyByapk8RRVvkVH4hLkVvvb08cX57H_9uwM";
+
+// enable files upload
+app.use(
+  fileUpload({
+    createParentPath: true,
+  })
+);
+
+//add other middleware
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => res.send("Hello World!"));
+
+app.post("/upload", async (req, res) => {
+  req.files.pdfFile.mv("/temp/" + req.files.pdfFile.name);
+  const parsedData = await parsePDF(req.files.pdfFile).then((result) =>
+    JSON.stringify(result)
+  );
+  res.send({ sucess: true, parsedData });
+});
+
+app.listen(port, () =>
+  console.log(`Example app listening at http://localhost:${port}`)
+);
