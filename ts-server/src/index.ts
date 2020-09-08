3017a7f75e4b54af49072e8f0da9a67ad0faa3b4
unknown <jrglvn@yahoo.com>
2020-09-08 22:43:17 +0200

    saving

--- ts-server/src/index.ts
+++ ts-server/src/index.ts
@@ -29,7 +29,7 @@ app.get("/", (req, res) => res.send("Hello World!"));
 app.post("/upload", async (req, res) => {
   const data = await parsePDF({
     file: req.files.pdfFile,
-    options: { kind: "TRADE_LICENSE" },
+    options: { kind: "ESTABLISHMENT_ID" },
   });
   const parsedData = JSON.stringify(data);
   res.send({ sucess: true, parsedData });
--- ts-server/src/vision.ts
+++ ts-server/src/vision.ts
@@ -4,7 +4,7 @@ const { ImageAnnotatorClient } = require("@google-cloud/vision").v1;
 type TInput = {
   file: { data: any };
   options: {
-    kind: "TRADE_LICENSE";
+    kind: "TRADE_LICENSE" | "VAT_CERTIFICATE" | "ESTABLISHMENT_ID";
   };
 };
 
@@ -32,39 +32,53 @@ export async function parsePDF(input: TInput) {
   const result = await batchAnnotateFiles();
   const fullTextArray = result[0].fullTextAnnotation.text.split(/\r?\n/);
 
-  const tradeLicense = {
-    license_number: "xxx",
-    company_name: "xxx",
-    expiry_date: "xxx",
-  };
+  switch (input.options.kind) {
+    case "TRADE_LICENSE": {
+      const tradeLicense = {
+        license_number: null,
+        company_name: null,
+        expiry_date: null,
+      };
 
-  const licenseRegex = /^[0-9]{5,6}$/;
-  for (let i = 0; i < 10; i++) {
-    const temp = fullTextArray[i].match(licenseRegex);
-    if (temp) {
-      tradeLicense.license_number = temp[0];
-      break;
+      const licenseRegex = /^\d{5,6}$/;
+      for (let i = 0; i < 10; i++) {
+        const temp = fullTextArray[i].match(licenseRegex);
+        if (temp) {
+          tradeLicense.license_number = temp[0];
+          break;
+        }
+      }
+
+      const nameRegex = /^.*Name$/;
+      for (let i = 0; i < 25; i++) {
+        const temp = fullTextArray[i].match(nameRegex);
+        if (temp) {
+          tradeLicense.company_name = fullTextArray[i + 1];
+          break;
+        }
+      }
+
+      const expiryRegex = /^..\/..\/....$/;
+      for (let i = 0; i < 30; i++) {
+        const temp = fullTextArray[i].match(expiryRegex);
+        if (temp) {
+          tradeLicense.expiry_date = temp[0];
+          break;
+        }
+      }
+
+      return { ...tradeLicense, fullTextArray };
+    }
+
+    case "ESTABLISHMENT_ID": {
+      const establishmentId = {
+        license_number: null,
+        company_name: null,
+        expiry_date: null,
+      };
+      return { ...establishmentId, fullTextArray };
     }
   }
 
-  const nameRegex = /^.*Name$/;
-  for (let i = 0; i < 25; i++) {
-    const temp = fullTextArray[i].match(nameRegex);
-    if (temp) {
-      tradeLicense.company_name = fullTextArray[i + 1];
-      break;
-    }
-  }
-
-  const expiryRegex = /^..\/..\/....$/;
-  for (let i = 0; i < 30; i++) {
-    const temp = fullTextArray[i].match(expiryRegex);
-    if (temp) {
-      tradeLicense.expiry_date = temp[0];
-      break;
-    }
-  }
-
-  return tradeLicense;
   // return result;
 }
